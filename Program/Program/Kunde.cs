2390bbb0901285b681ce1277012e25179c80f16c
Oliver Richter Klitgaard <oliv6157@elev.tec.dk>
2018-12-11 13:01:30 +0100

    Kunde og SQL

--- Program/Program/Kunde.cs
+++ Program/Program/Kunde.cs
@@ -8,5 +8,58 @@ namespace Program
 {
 	class Kunde
 	{
+		public string Navn { get; set; }
+		public string Efternavn { get; set; }
+		public string Adr { get; set; }
+		public int Postnr { get; set; }
+		public int Tlf { get; set; }
+		public string OpretDato { get; set; }
+		public string Email { get; set; }
+
+		private static void opdaterKunde()
+		{
+
+		}
+		public void sletKunde()
+		{
+			Console.WriteLine("Kundeliste:\n");
+			SQL.SelectFewKunde("select KundeId, Navn, Efternavn from Kunder");
+			Console.WriteLine();
+			Console.Write("Indtast ID på kunden der skal slettes: ");
+			int idValg = Convert.ToInt32(Console.ReadLine());
+			SQL.Change("delete from Kunder where KundeId = " + idValg + "");
+			SQL.SelectFewKunde("select KundeId, Navn, Efternavn from Kunder");
+
+		}
+		public void opretKunde()
+		{
+			Console.Clear();
+			Console.WriteLine("Indtast Navn: ");		
+			Console.WriteLine("Indtast Efternavn: ");
+			Console.WriteLine("Indtast adresse: ");
+			Console.WriteLine("Indtast Postnummer: ");
+			Console.WriteLine("Intast telefonnummer: ");
+			Console.WriteLine("Indtast E-Mail adresse: ");
+			Navn = Console.ReadLine();
+			Efternavn = Console.ReadLine();			
+			Adr = Console.ReadLine();
+			Postnr = Convert.ToInt32(Console.ReadLine());
+			Tlf = Convert.ToInt32(Console.ReadLine());
+			Email = Console.ReadLine();
+			OpretDato = DateTime.Now.ToString("d");
+			SQL.Change("insert into Kunder values('" + Navn + "', '" + Efternavn + "', '" + Adr + "', " + Postnr + ", " + Tlf + ",'" + OpretDato + "', '" + Email + "')"); 
+
+			
+		}
+		//public Kunde(string navn, string efternavn, string adresse, int postnr, int tlf, string email, DateTime opretDato)
+		//{
+		//	Navn = navn;
+		//	Efternavn = efternavn;
+		//	Adr = adresse;
+		//	Postnr = postnr;
+		//	Tlf = tlf;
+		//	Email = email;
+		//	OpretDato = opretDato;
+		//}
 	}
 }
--- Program/Program/SQL.cs
+++ Program/Program/SQL.cs
@@ -3,10 +3,117 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace Program
 {
     class SQL
     {
-    }
+		public static void Change(string sql)
+		{
+			using (SqlConnection con = new SqlConnection(SQLcon.SQLconnection))
+			{
+				con.Open();
+				SqlCommand cmd = new SqlCommand(sql, con);
+				cmd.ExecuteNonQuery();
+			}
+		}
+		public static void SelectAllKunde(string sql)
+		{
+			DataTable table = new DataTable();
+			using (SqlConnection con = new SqlConnection(SQLcon.SQLconnection))
+			{
+				con.Open();
+				SqlDataAdapter adapter = new SqlDataAdapter(sql, con);
+				adapter.Fill(table);
+
+				foreach (DataRow item in table.Rows)
+				{
+					Console.Write(item["KundeId"].ToString() + ". ");
+					Console.Write(item["Navn"].ToString() + " ");
+					Console.Write(item["EfterNavn"].ToString() + " ");
+					Console.Write(item["Adresse"].ToString() + " ");
+					Console.Write(item["PostNr"].ToString() + " ");
+					Console.Write(item["Tlf"].ToString() + " ");
+					Console.Write(item["OpretDato"].ToString() + " ");
+					Console.Write(item["Email"].ToString() + " ");
+
+
+					Console.WriteLine();
+				}
+			}
+
+		}
+		public static void SelectFewKunde(string sql)
+		{
+			DataTable table = new DataTable();
+			using (SqlConnection con = new SqlConnection(SQLcon.SQLconnection))
+			{
+				con.Open();
+				SqlDataAdapter adapter = new SqlDataAdapter(sql, con);
+				adapter.Fill(table);
+
+				foreach (DataRow item in table.Rows)
+				{
+					Console.Write(item["KundeId"].ToString() + ". ");
+					Console.Write(item["Navn"].ToString() + " ");
+					Console.Write(item["EfterNavn"].ToString() + " ");
+					Console.WriteLine();
+				}
+			}
+
+		}
+		public static void SelectBil(string sql)
+		{
+			DataTable table = new DataTable();
+			using (SqlConnection con = new SqlConnection(SQLcon.SQLconnection))
+			{
+				con.Open();
+				SqlDataAdapter adapter = new SqlDataAdapter(sql, con);
+				adapter.Fill(table);
+
+				foreach (DataRow item in table.Rows)
+				{
+					Console.Write(item["KundeId"].ToString() + " ");
+					Console.Write(item["BilId"].ToString() + " ");
+					Console.Write(item["Mærke"].ToString() + " ");
+					Console.Write(item["Model"].ToString() + " ");
+					Console.Write(item["Årgang"].ToString() + " ");
+					Console.Write(item["KmKørt"].ToString() + " ");
+					Console.Write(item["Brændstof"].ToString() + " ");
+					Console.Write(item["RegNr"].ToString() + " ");
+
+
+
+					Console.WriteLine();
+				}
+			}
+
+		}
+		public static void SelectBesøg(string sql)
+		{
+			DataTable table = new DataTable();
+			using (SqlConnection con = new SqlConnection(SQLcon.SQLconnection))
+			{
+				con.Open();
+				SqlDataAdapter adapter = new SqlDataAdapter(sql, con);
+				adapter.Fill(table);
+
+				foreach (DataRow item in table.Rows)
+				{
+					Console.Write(item["KundeId"].ToString() + " ");
+					Console.Write(item["BilId"].ToString() + " ");
+					Console.Write(item["FakturaNr"].ToString() + " ");
+					Console.Write(item["Dato"].ToString() + " ");
+					Console.Write(item["Udført"].ToString() + " ");
+
+
+					Console.WriteLine();
+				}
+			}
+
+		}
+
+	}
 }
